3e56f8a643db0d0d76fec403f1873f1e08a61134
rjadrich <rjadrich@gmail.com>
2016-04-13 15:31:53 -0500

    started converting potentials to class members

--- code_and_scripts/code/potentials/potentials.h
+++ code_and_scripts/code/potentials/potentials.h
@@ -1,8 +1,42 @@
 #ifndef POTENTIALS_H
 #define POTENTIALS_H
 
-//SPECIFIC POTENTIAL FUNCTION DEFINITIONS
-//THE POTENTIALS ARE STORED IN SEPARATE CPP FILES FOR CONVENIENCE
+///////////////////////////////////////////////////////
+//////CLASS DEFINITION FOR THE VARIOUS POTENTIALS//////
+///////////////////////////////////////////////////////
+/*class potential
+{
+private:
+	int num_parameters;
+	int num_d_parameters;
+	double *potential_parameters; 
+	double *d_potential_parameters; 
+
+public:
+	void set_potential(int potential_type)
+	{
+		if (potential_type == 0) //ideal_cluster_potential
+		{
+			num_parameters = 6;
+			num_d_parameters = 4;
+		}
+		if (potential_type == 1) //ramp_salr_cluster_potential
+		{
+			num_parameters = 6;
+			num_d_parameters = 4;
+		}
+		
+		//ALLOCATE MEMORY FOR READING IN THE PARAMETERS
+		potential_parameters = (double*)malloc(sizeof(double) * num_parameters);
+		d_potential_parameters = (double*)malloc(sizeof(double) * num_d_parameters);
+	}
+};*/
+
+///////////////////////////////////////////////////////
+///////SPECIFIC POTENTIAL FUNCTION DEFINITIONS/////////
+///////////////////////////////////////////////////////
+
+//THE POTENTIALS ARE STORED IN INDIVIDUAL CPP FILES FOR CONVENIENCE
 
 //from ideal_cluster_potential.cpp
 array_pair_and_num_elements optimize_ideal_cluster_potential(int last_step, array_pair_and_num_elements gr_data,
